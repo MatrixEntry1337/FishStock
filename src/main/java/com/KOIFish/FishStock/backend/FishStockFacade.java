77aab335206bd660bf682bddc388c1128dc33242
oiale <ilysiarh@gmail.com>
2016-12-29 17:05:41 -0500

    Initial commit for the branch

--- src/main/java/com/KOIFish/FishStock/backend/FishStockFacade.java
+++ src/main/java/com/KOIFish/FishStock/backend/FishStockFacade.java
@@ -1,5 +1,12 @@
 package com.KOIFish.FishStock.backend;
 
-public class FishStockFacade {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class FishStockFacade {
+	@Autowired
+	private FishStockUserDAO userDAO;
+	
+	
 }
