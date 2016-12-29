2c1c4ed468e337fbd86a401e3ff3c4ddfca6512e
Osher Cohen <osher.y.cohen@gmail.com>
2016-12-29 15:03:11 -0500

    Finally fixed the merge

---
+++ src/main/resources/Rating.feature
@@ -0,0 +1,10 @@
+Feature: rating
+  As a user
+  I can rate stocks
+
+  Scenario:
+    Given I am logged in
+    And I am viewing a stock
+    When I select a rating for the stock
+    And I click the submit button
+    Then the rating I have selected will be submitted
---
+++ src/test/java/com/KOIFish/FishStock/Rating/RatingStepDefinitions.java
@@ -0,0 +1,38 @@
+package com.KOIFish.FishStock.Rating;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RatingStepDefinitions {
+    @And("^I click the submit button$")
+    public void iClickTheSubmitButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am logged in$")
+    public void iAmLoggedIn() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I am viewing a stock$")
+    public void iAmViewingAStock() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I select a rating for the stock$")
+    public void iSelectARatingForTheStock() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the rating I have selected will be submitted$")
+    public void theRatingIHaveSelectedWillBeSubmitted() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
