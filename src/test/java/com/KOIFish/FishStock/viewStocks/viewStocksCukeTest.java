28ca8622c20b83d2d1e6c86ecfb083857b788cf6
Kyle Chang Fatt <kchangfatt@gmail.com>
2016-12-29 12:56:10 -0500

    Created the test class for the view stock feature, stubs were created from feature file

---
+++ src/test/java/com/KOIFish/FishStock/viewStocks/viewStocksCukeTest.java
@@ -0,0 +1,36 @@
+package com.KOIFish.FishStock.viewStocks;
+
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class viewStocksCukeTest {
+
+	@Given("^I have these stocks$")
+	public void i_have_these_stocks(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@Given("^I am logged in$")
+	public void i_am_logged_in() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^I view <stocks> stocks$")
+	public void i_view_stocks_stocks() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should see all <stockList> stocks$")
+	public void i_should_see_all_stockList_stocks() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
