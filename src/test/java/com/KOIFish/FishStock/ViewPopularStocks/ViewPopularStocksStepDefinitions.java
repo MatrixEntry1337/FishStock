bebd00a6f1ac9558a4cb8ee395f4831b1ba1b2a1
Osher Cohen <osher.y.cohen@gmail.com>
2016-12-29 14:32:27 -0500

    Pom file now matches master

---
+++ src/test/java/com/KOIFish/FishStock/ViewPopularStocks/ViewPopularStocksStepDefinitions.java
@@ -0,0 +1,33 @@
+package com.KOIFish.FishStock.ViewPopularStocks;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+public class ViewPopularStocksStepDefinitions {
+    @And("^I am viewing stocks$")
+    public void iAmViewingStocks() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am logged in$")
+    public void iAmLoggedIn() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click to see the most popular stocks$")
+    public void iClickToSeeTheMostPopularStocks() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the most popular stocks appear$")
+    public void theMostPopularStocksAppear() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
