6d09f7a6e2862dee16a69e5d0f85a5644f9e9689
Kyle Chang Fatt <kchangfatt@gmail.com>
2016-12-31 00:40:34 -0500

    Added interface method for getting all Companies

--- src/main/java/com/KOIFish/FishStock/backend/FishStockCompanyDAO.java
+++ src/main/java/com/KOIFish/FishStock/backend/FishStockCompanyDAO.java
@@ -1,12 +1,16 @@
 package com.KOIFish.FishStock.backend;
 
+import java.util.Set;
+
 import org.hibernate.Session;
+
+import com.KOIFish.FishStock.beans.FishStockCompany;
 
 /**
  * Created by yehur on 12/30/2016.
  */
 public interface FishStockCompanyDAO {
     int getAverageRatingByCompanyId(Session session, int id);
-
-
+    Set<FishStockCompany> getAllCompanies(Session session);
 }
+
