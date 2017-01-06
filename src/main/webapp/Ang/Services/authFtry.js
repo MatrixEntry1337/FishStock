b8d5b9663ab9de39bf6064132d14bd02ef076407
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-06 09:14:04 -0500

    changed how passed functions are consumed.

--- src/main/webapp/Ang/Services/authFtry.js
+++ src/main/webapp/Ang/Services/authFtry.js
@@ -6,35 +6,25 @@ angular.module('auth')
 .factory('authFtry', function($http, $log){
 	$log.log("Started Auth Factory");
 	
-	var public = {};
+	auth = {};
 	
-	var data = {
-			appName: "FishStock",
-			authors : "Kyle Chang Fatt, Osher Cohen, Ilya Siarheyeu",
-			about: "This is a stock watch application built using Java EE and AngularJS",
-			version: 0.1
+	auth.loggedIn = false;
+	
+	auth.checkLogin = function(){
+		return loggedIn();
 	};
 	
-	
-	public.login = function (loginFormData, successFunc, failureFunc) {
-		$http.post('/FishStock/login.do', loginFormData).then(successFunc, failureFunc);
+	auth.login = function (loginFormData, successFunc, failureFunc) {
+		$http.post('/FishStock/login.do', loginFormData)
+		.then(function(response){
+			successFunc(response);
+			auth.loggedIn = true;
+		})
+		.catch(function(response){
+			failureFunc(response);
+		})
+		.finally($log.log("Always runs"));
 	};
 	
-	public.getAppName = function(){
-		return data.appName;
-	};
-	
-	public.getAuthors = function(){
-		return data.authors
-	};
-	
-	public.getAbout = function(){
-		return data.about;
-	};
-	
-	public.getVersion = function(){
-		return data.version;
-	};
-	
-	return public;
+	return auth;
 });
