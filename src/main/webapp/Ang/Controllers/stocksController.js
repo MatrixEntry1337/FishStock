9b602bf8eace3d196796e4b0162b0de93388db5a
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-06 09:58:35 -0500

    Changed UI for home page and all stocks page

--- src/main/webapp/Ang/Controllers/stocksController.js
+++ src/main/webapp/Ang/Controllers/stocksController.js
@@ -1,6 +1,8 @@
 angular.module('stocks')
 .controller('stocksCtrl', function(stocksFtry, $scope, $log){
 	$scope.getAll = stocksFtry.getAll();
-	
 	$scope.allStocks = stocksFtry.allStocks;
+	$scope.selectStock = function(stock){
+		$scope.selectedStock = stock;
+	};
 });
