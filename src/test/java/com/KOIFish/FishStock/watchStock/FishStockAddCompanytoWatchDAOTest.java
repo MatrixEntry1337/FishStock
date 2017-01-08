452ebd3e121f404af7918fe496c74817bc95a08e
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-07 23:54:09 -0500

    Created test for adding a company to watch list for a given user

--- src/test/java/com/KOIFish/FishStock/watchStock/FishStockAddCompanytoWatchDAOTest.java
+++ src/test/java/com/KOIFish/FishStock/watchStock/FishStockAddCompanytoWatchDAOTest.java
@@ -1,5 +1,9 @@
 package com.KOIFish.FishStock.watchStock;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -7,6 +11,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.KOIFish.FishStock.backend.FishStockTimePeriodDAO;
 import com.KOIFish.FishStock.beans.FishStockCompany;
+import com.KOIFish.FishStock.beans.FishStockTimeperiod;
 import com.KOIFish.FishStock.beans.FishStockUser;
 
 public class FishStockAddCompanytoWatchDAOTest {
@@ -15,17 +20,43 @@ public class FishStockAddCompanytoWatchDAOTest {
 	private static ApplicationContext context;
 	
 	@BeforeClass
-	public void preClass(){
-		context = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/beans/xml");
+	public static void preClass(){
+		context = new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/beans.xml");
 	}
 	
 	@Test
-	public void watchStockTest(){
+	public void addStockToWatchTest(){
 		FishStockTimePeriodDAO dao = context.getBean("timePeriodDAO", FishStockTimePeriodDAO.class);
+		
+		// create user
 		FishStockUser user = new FishStockUser();
+		user.setId(1);
+		user.setUsername("thanks");
+		
+		// create company
 		FishStockCompany company = new FishStockCompany();
+		company.setCompanyId(3);
+		company.setSymbol("CSCO");
+		
+		// remove and add company
+		dao.removeCompanyFromWatch(user, company);
 		dao.addCompanytoWatch(user, company);
 		
+		
+		FishStockTimeperiod tp = new FishStockTimeperiod();
+		tp.setUser(user);
+		tp.setCompany(company);
+		Set<FishStockTimeperiod> set =  dao.getAllWatchList();
+		assertTrue(set.contains(tp));
 	}
 	
+	@Test(expected=Exception.class)
+	public void addStockToWatchUniqueViolationTest() {
+		FishStockTimePeriodDAO dao = context.getBean("timePeriodDAO", FishStockTimePeriodDAO.class);
+		FishStockUser user = new FishStockUser();
+		user.setId(1);
+		FishStockCompany company = new FishStockCompany();
+		company.setCompanyId(9);
+		dao.addCompanytoWatch(user, company);
+	}
 }
