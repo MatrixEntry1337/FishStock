8c12a92d84761f4118856d6fb1c714aecfda89c6
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-07 23:52:19 -0500

    Generated Hash and Equals for beans so that the contains method for the Set Collection could be used.

--- src/main/java/com/KOIFish/FishStock/beans/FishStockTimeperiod.java
+++ src/main/java/com/KOIFish/FishStock/beans/FishStockTimeperiod.java
@@ -47,4 +47,36 @@ public class FishStockTimeperiod {
 	
 	public Timestamp getEndDate() { return endDate; }
 	public void setEndDate(Timestamp endDate) { this.endDate = endDate; }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FishStockTimeperiod other = (FishStockTimeperiod) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	
 }
--- src/main/java/com/KOIFish/FishStock/beans/FishStockUser.java
+++ src/main/java/com/KOIFish/FishStock/beans/FishStockUser.java
@@ -76,5 +76,30 @@ public class FishStockUser {
 		return "FishStockUser [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", username=" + username + ", password=" + password + ", companiesWatched=" + companiesWatched + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FishStockUser other = (FishStockUser) obj;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 	
 }
