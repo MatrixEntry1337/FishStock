cbf1c43a28ead93cb50f4cb77cd474dbb37abe07
Kyle Chang Fatt <kchangfatt@gmail.com>
2017-01-12 10:46:45 -0500

    Fixed resolve for user data

--- src/main/webapp/Ang/Services/accountFtry.js
+++ src/main/webapp/Ang/Services/accountFtry.js
@@ -4,15 +4,19 @@ angular.module('account').factory('accountFtry', function($http, $log){
 	account.data;
 	
 	account.getUserData = function(){
+		var data = {};
+		
 		$http.get('/FishStock/getuser.do')
 			.then(function(response){
-				$log.log("Get all -- response object: ");
+				$log.log("Get user data -- response object: ");
 				$log.log(response.data);
 				account.data = response.data;
+				angular.copy(response.data, data);
 			})
 			.catch(function(response){
 				$log.log("There was an error, error status: " + response.status);
 			});
+		return data;
 	};
 	
 	return account;
